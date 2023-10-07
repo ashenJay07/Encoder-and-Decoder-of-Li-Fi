ddc1e62588d4dc713fa6cc77543ef1cb6368a0b4
Ashen Jayarathne <98721130+ashenJay07@users.noreply.github.com>
2023-10-07 11:31:15 +0530

    Encode the image by using Base64 encoding scheme.

--- LiFi-Tx/main.py
+++ LiFi-Tx/main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import base64
+
 
 def open_image():
     file_path = filedialog.askopenfilename()
@@ -14,6 +16,13 @@ def open_image():
         photo = ImageTk.PhotoImage(image)
         image_label.config(image=photo)
         image_label.photo = photo  # Prevent garbage collection
+
+        # Encode the image in base64
+        with open(file_path, "rb") as image_file:
+            base64_image = base64.b64encode(image_file.read()).decode("utf-8")
+        print("Base64 encoded image:")
+        print(base64_image)
+
 
 app = tk.Tk()
 app.title("Image Viewer")
